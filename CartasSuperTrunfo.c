f4a6c77003548d0818c4925d86ea667ec2133e82
guttonerdfacul <guttonerd.facul@gmail.com>
2025-02-24 21:55:13 +0000

    Mudanca de "Float" para "Double"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,13 +11,13 @@ int main()
     // Variáveis
     char pais[7] = "BRASIL";
     char c_estado1, c_estado2;
-    char n_estado1[30], n_estado2[30];          // Nome do estado com 1 caractere de A a H
-    char c_cidade1[5], c_cidade2[5];            // O código da cidade deverá ser o código do estado (de A a Z) + um número de 01 a 04 (com 2 dígitos)
-    char n_cidade1[30], n_cidade2[30];          // Nome da cidade 
-    int pop1, pop2;                             // População
-    float area1, area2;                         // Área
-    float pib1, pib2;                           // PIB
-    int p_tur1, p_tur2;                         // Número de Pontos turísticos
+    char n_estado1[30], n_estado2[30];              // Nome do estado com 1 caractere de A a H
+    char c_cidade1[5], c_cidade2[5];                // O código da cidade deverá ser o código do estado (de A a Z) + um número de 01 a 04 (com 2 dígitos)
+    char n_cidade1[30], n_cidade2[30];              // Nome da cidade 
+    int pop1, pop2;                                 // População
+    double area1, area2;                            // Área (mudado de float para double)
+    double pib1, pib2;                              // PIB (mudado de float para double)
+    int p_tur1, p_tur2;                             // Número de Pontos turísticos
 
     
     // ENTRADA DE DADOS DA PRIMEIRA CARTA
@@ -35,9 +35,9 @@ int main()
     printf("* Qual a POPULAÇÃO de ** %s **: ", n_cidade1);
     scanf(" %d", &pop1);
     printf("* Qual a ÁREA de ** %s **: ", n_cidade1);
-    scanf(" %f", &area1);
+    scanf(" %lf", &area1);                          // Mudado de %f para %lf
     printf("* Qual o PIB de ** %s **: ", n_cidade1);
-    scanf(" %f", &pib1);
+    scanf(" %lf", &pib1);                           // Mudado de %f para %lf
     printf("* Quantos pontos turísticos tem ** %s **: ", n_cidade1);
     scanf("%d", &p_tur1);
 
@@ -62,9 +62,9 @@ int main()
     printf("* Qual a POPULAÇÃO de ** %s **: ", n_cidade2);
     scanf(" %d", &pop2);
     printf("* Qual a ÁREA de ** %s **: ", n_cidade2);
-    scanf(" %f", &area2);
+    scanf(" %lf", &area2);                          // Mudado de %f para %lf
     printf("* Qual o PIB de ** %s **: ", n_cidade2);
-    scanf(" %f", &pib2);
+    scanf(" %lf", &pib2);                           // Mudado de %f para %lf
     printf("* Quantos pontos turísticos tem ** %s **: ", n_cidade2);
     scanf("%d", &p_tur2);
 
@@ -75,18 +75,15 @@ int main()
     
     // CONFIRMAÇÃO DOS DADOS INSERIDOS
     // CARTA 1
-
-    printf("As informaçoes inseridas são:\n");
-    printf(" \n");          
-    printf(" \n");
+    
     printf(" ----- CARTA 1 ----- \n");
     printf(" \n");
     printf("Estado: %c - %s\n", c_estado1, n_estado1);
     printf("Código: %s\n", c_cidade1);
     printf("Nome da Cidade: %s\n", n_cidade1);
     printf("População: %d habitantes\n", pop1);  
-    printf("Área: %.2f Km²\n", area1);  
-    printf("PIB: %.2f bilhões de Reais\n", pib1);  
+    printf("Área: %.2f Km²\n", area1);              // %f continua funcionando para double
+    printf("PIB: %.2f bilhões de Reais\n", pib1);   // %f continua funcionando para double
     printf("Quantidade de Pontos Turísticos: %d \n", p_tur1);
 
     // PULANDO ALGUMAS LINHAS
@@ -102,14 +99,10 @@ int main()
     printf("Estado: %c - %s\n", c_estado2, n_estado2);
     printf("Código: %s\n", c_cidade2);
     printf("Nome da Cidade: %s\n", n_cidade2);
-    printf("População: %d habitantes\n", pop2);  // Corrigido de %c para %d
-    printf("Área: %.2f Km²\n", area2);  // Adicionado formatação para 2 casas decimais
-    printf("PIB: %.2f bilhões de Reais\n", pib2);  // Adicionado formatação para 2 casas decimais
+    printf("População: %d habitantes\n", pop2);  
+    printf("Área: %.2f Km²\n", area2);              // %f continua funcionando para double
+    printf("PIB: %.2f bilhões de Reais\n", pib2);   // %f continua funcionando para double
     printf("Quantidade de Pontos Turísticos: %d \n", p_tur2);
-
-    printf(" \n");          
-    printf(" \n");
-    printf(" \n");
 
     return 0;
 }
